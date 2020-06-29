8a550cfb644d772f84f2504bdfc0a117d531fa18
Aspodel D <dattran2309@gmail.com>
2020-06-29 11:00:12 +0700

    version 1.0.0

---
+++ TestAuthen/Controllers/UserProfileController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TestAuthen.Models;
+
+namespace TestAuthen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+
+        public UserProfileController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<Object> GetUserProfile()
+        {
+            string userId = User.Claims.First(c => c.Type == "UserId").Value;
+            var user = await _userManager.FindByIdAsync(userId);
+            return new
+            {
+                user.FullName,
+                user.Email,
+                user.UserName
+            };
+        }
+    }
+}
